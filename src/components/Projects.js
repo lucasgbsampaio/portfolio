f79d119b7ea5aae14875859390668c52fa75534a
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2021-01-07 11:24:37 -0300

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import chatapp from '../assets/images/chatapp.png';
+import toDoList from '../assets/images/to-do.png';
+import weatherabove from '../assets/images/weatherabove.png';
+import dogs from '../assets/images/dogs.png';
+
 import style from './styles/Projects.module.css';
 
 export default function Projects() {
@@ -10,7 +15,9 @@ export default function Projects() {
       </header>
       <ul className={style.features}>
         <li>
-          <img className={style.image} src={{}} alt="" />
+          <div className={style.image}>
+            <img src={chatapp} alt="chatapp" />
+          </div>
           <h3>Chat App</h3>
           <p>
             Aplicativo de salas de conversas com comunicação em tempo real
@@ -27,7 +34,9 @@ export default function Projects() {
           </footer>
         </li>
         <li>
-          <img className={style.image} src={{}} alt="" />
+          <div className={style.image}>
+            <img src={weatherabove} alt="weatherabove" />
+          </div>
           <h3>WeatherAbove</h3>
           <p>
             Aplicação de previsão do tempo que apresenta dados meteorológicos de
@@ -43,7 +52,9 @@ export default function Projects() {
           </footer>
         </li>
         <li>
-          <img className={style.image} src={{}} alt="" />
+          <div className={style.image}>
+            <img src={dogs} alt="dogs" />
+          </div>
           <h3>Dogs</h3>
           <p>
             Rede social para cachorros no estilo do Instagram. É possível postar
@@ -59,7 +70,9 @@ export default function Projects() {
           </footer>
         </li>
         <li>
-          <img className={style.image} src={{}} alt="" />
+          <div className={style.image}>
+            <img src={toDoList} alt="toDoList" />
+          </div>
           <h3>To-Do App</h3>
           <p>
             Site de registro de tarefas para obter controle e planejamento ao
@@ -75,7 +88,9 @@ export default function Projects() {
           </footer>
         </li>
         <li>
-          <img className={style.image} src={{}} alt="" />
+          <div className={style.image}>
+            <img src={chatapp} alt="chatapp" />
+          </div>
           <h3>Portfólio</h3>
           <p>Meu Portfólio feito com React, totalmente responsivo.</p>
           <footer className={style.footerImage}>
@@ -88,7 +103,9 @@ export default function Projects() {
           </footer>
         </li>
         <li>
-          <img className={style.image} src={{}} alt="" />
+          <span className={style.image}>
+            <img src={chatapp} alt="chatapp" />
+          </span>
           <h3>Happy</h3>
           <p>
             Feito no dia das Crianças, é um aplicativo de registro de
