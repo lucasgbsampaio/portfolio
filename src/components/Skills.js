63d00d11000b74f2b00c4cdd297f597addc87a0b
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2021-01-08 15:18:28 -0300

    update component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -24,41 +24,31 @@ export default function Skills() {
           <span>
             <SiHtml5 />
           </span>
-          <strong />
-          HTML5
-          <strong />
+          <strong>HTML5</strong>
         </li>
         <li className={style.style2}>
           <span>
             <SiCss3 />
           </span>
-          <strong />
-          CSS3
-          <strong />
+          <strong>CSS3</strong>
         </li>
         <li className={style.style3}>
           <span>
             <SiJavascript />
           </span>
-          <strong />
-          Javascript
-          <strong />
+          <strong>JavaScript</strong>
         </li>
         <li className={style.style4}>
           <span>
             <SiGit />
           </span>
-          <strong />
-          Git
-          <strong />
+          <strong>Git</strong>
         </li>
         <li className={style.style5}>
           <span>
             <SiBootstrap />
           </span>
-          <strong />
-          Bootstrap
-          <strong />
+          <strong>Bootstrap</strong>
         </li>
       </ul>
       <ul className={style.skills}>
@@ -66,41 +56,31 @@ export default function Skills() {
           <span>
             <SiReact />
           </span>
-          <strong />
-          React
-          <strong />
+          <strong>React</strong>
         </li>
         <li className={style.style4}>
           <span>
             <FaNodeJs />
           </span>
-          <strong />
-          Express.js
-          <strong />
+          <strong>Express.js</strong>
         </li>
         <li className={style.style3}>
           <span>
             <FaNodeJs />
           </span>
-          <strong />
-          Node.js
-          <strong />
+          <strong>Node.js</strong>
         </li>
         <li className={style.style2}>
           <span>
             <SiMongodb />
           </span>
-          <strong />
-          MongoDB
-          <strong />
+          <strong>MongoDB</strong>
         </li>
         <li className={style.style1}>
           <span>
             <SiPostgresql />
           </span>
-          <strong />
-          PostgreSQL
-          <strong />
+          <strong>PostgreSQL</strong>
         </li>
       </ul>
     </section>
